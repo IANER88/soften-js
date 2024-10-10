a4fb91f23b9dcaeabdea12a424558eba84befb59
L.Jay <bozhancom@163.com>
2024-10-11 05:27:02 +0800

    fix: update return

--- src/use/use-dark-scheme.ts
+++ src/use/use-dark-scheme.ts
@@ -1,14 +1,10 @@
 import { useSignal } from ".";
 
 export default function useDarkScheme() {
-
   const dark = matchMedia("(prefers-color-scheme: dark)");
-
   const scheme = useSignal(dark?.matches ? 'dark' : 'lignt')
-
   dark.onchange = (event) => {
     scheme.value = event?.matches ? 'dark' : 'lignt'
   }
-
   return scheme;
 }
